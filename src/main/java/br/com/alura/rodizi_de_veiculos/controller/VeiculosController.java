8eb8baa8a034fa35f7845e12f2b9d39b3bb028b1
Rafael <rafa.s.francisco@gmail.com>
2021-12-18 11:55:18 -0300

    simplificando a ordenação

--- src/main/java/br/com/alura/rodizi_de_veiculos/controller/VeiculosController.java
+++ src/main/java/br/com/alura/rodizi_de_veiculos/controller/VeiculosController.java
@@ -4,9 +4,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,12 +41,9 @@ public class VeiculosController {
 		return new VeiculoRes(service.criar(veiculoDto.toVeiculo()).get());
 	}
 	
+	// localhost:8080/veiculos?page=0&size=5&sort=placa,asc
 	@GetMapping
-	public Page<VeiculoRes> listar(@RequestParam int pagina,
-									@RequestParam int quantidade,
-									@RequestParam String ordenacao) {
-		
-		Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.ASC, ordenacao);
+	public Page<VeiculoRes> listar(@PageableDefault(sort = "placa") Pageable paginacao) {
 		
 		return VeiculoRes.converterParaPage(service.lista(paginacao).get());
 	}
