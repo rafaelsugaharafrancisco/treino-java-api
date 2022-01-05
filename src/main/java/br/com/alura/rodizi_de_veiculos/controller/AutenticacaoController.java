c590fd138c9534ec42f0e90f3fadd08ab7cea8b2
Rafael <rafa.s.francisco@gmail.com>
2022-01-05 01:10:24 -0300

    utilização de profiles

--- src/main/java/br/com/alura/rodizi_de_veiculos/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/rodizi_de_veiculos/controller/AutenticacaoController.java
@@ -3,6 +3,7 @@ package br.com.alura.rodizi_de_veiculos.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -19,6 +20,7 @@ import br.com.alura.rodizi_de_veiculos.service.TokenService;
 
 @RestController
 @RequestMapping("/auth")
+@Profile("prod")
 public class AutenticacaoController {
 	
 	private AuthenticationManager authManager;
